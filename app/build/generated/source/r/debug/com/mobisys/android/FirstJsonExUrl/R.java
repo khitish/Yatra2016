503906646c52c254f5990c3efa0addef07b549bc
vikash <vikash1992.11@gmail.com>
2016-04-02 10:40:17 +0530

    added successfully

--- app/build/generated/source/r/debug/com/mobisys/android/FirstJsonExUrl/R.java
+++ app/build/generated/source/r/debug/com/mobisys/android/FirstJsonExUrl/R.java
@@ -36,29 +36,32 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
         public static final int icon=0x7f020004;
         public static final int topbg=0x7f020005;
+        public static final int white_button=0x7f020006;
     }
     public static final class id {
-        public static final int Email=0x7f09000f;
-        public static final int Email_Id=0x7f090007;
-        public static final int PassWord=0x7f090008;
-        public static final int Repassword=0x7f090011;
-        public static final int action_settings=0x7f090014;
-        public static final int button=0x7f090009;
-        public static final int button3=0x7f09000a;
+        public static final int Email=0x7f090011;
+        public static final int Email_Id=0x7f090009;
+        public static final int PassWord=0x7f09000a;
+        public static final int Repassword=0x7f090013;
+        public static final int action_settings=0x7f090016;
+        public static final int button=0x7f09000b;
+        public static final int button2=0x7f090005;
+        public static final int button3=0x7f09000c;
         public static final int content_frame=0x7f090003;
         public static final int drawer_layout=0x7f090002;
         public static final int drawer_list=0x7f090004;
-        public static final int lblListItem=0x7f090006;
+        public static final int lblListItem=0x7f090008;
         public static final int listView=0x7f090001;
-        public static final int list_view=0x7f090005;
-        public static final int name=0x7f090012;
-        public static final int password=0x7f090010;
-        public static final int poi_budget=0x7f09000e;
-        public static final int poi_img=0x7f09000b;
-        public static final int poi_name=0x7f09000d;
-        public static final int poi_rank=0x7f09000c;
+        public static final int list_view=0x7f090007;
+        public static final int name=0x7f090014;
+        public static final int password=0x7f090012;
+        public static final int poi_budget=0x7f090010;
+        public static final int poi_img=0x7f09000d;
+        public static final int poi_name=0x7f09000f;
+        public static final int poi_rank=0x7f09000e;
         public static final int searchfield=0x7f090000;
-        public static final int signup=0x7f090013;
+        public static final int signup=0x7f090015;
+        public static final int tv_content=0x7f090006;
     }
     public static final class layout {
         public static final int actionbar_view=0x7f030000;
@@ -67,11 +70,12 @@ public final class R {
         public static final int activity_plan=0x7f030003;
         public static final int activity_poi=0x7f030004;
         public static final int drawer_list_item=0x7f030005;
-        public static final int fragment_plan_trip=0x7f030006;
-        public static final int list_item=0x7f030007;
-        public static final int login_activity=0x7f030008;
-        public static final int place_to_visit=0x7f030009;
-        public static final int signup_activity=0x7f03000a;
+        public static final int fragment_layout=0x7f030006;
+        public static final int fragment_plan_trip=0x7f030007;
+        public static final int list_item=0x7f030008;
+        public static final int login_activity=0x7f030009;
+        public static final int place_to_visit=0x7f03000a;
+        public static final int signup_activity=0x7f03000b;
     }
     public static final class menu {
         public static final int menu_home__acitivity=0x7f080000;
